9ddc0b697f538159a2166c30eb18d1a7137a986f
Abdoul93230 <117718513+Abdoul93230@users.noreply.github.com>
2024-12-16 16:25:23 -0800

    Update src/App.jsx [BoltSync]

--- src/App.jsx
+++ src/App.jsx
@@ -17,34 +17,37 @@ import AdminCareers from './admin/pages/AdminCareers';
 import AdminTeam from './admin/pages/AdminTeam';
 import AdminSettings from './admin/pages/AdminSettings';
 import Footer from './components/Footer';
+import { AdminProvider } from './contexts/AdminContext';
 
 export default function App() {
   return (
     <Router>
-      <div className="relative">
-        <Routes>
-          {/* Routes publiques */}
-          <Route path="/" element={<><Navbar /><Home /><Footer /></>} />
-          <Route path="/products" element={<><Navbar /><Products /><Footer /></>} />
-          <Route path="/product/:id" element={<><Navbar /><ProductDetails /><Footer /></>} />
-          <Route path="/research" element={<><Navbar /><Research /><Footer /></>} />
-          <Route path="/careers" element={<><Navbar /><Careers /><Footer /></>} />
-          <Route path="/team" element={<><Navbar /><Team /><Footer /></>} />
-          <Route path="/blog" element={<><Navbar /><Blog /><Footer /></>} />
-          <Route path="/blog/:id" element={<><Navbar /><BlogPost /><Footer /></>} />
-          <Route path="/job/:id" element={<><Navbar /><JobDetails /><Footer /></>} />
+      <AdminProvider>
+        <div className="relative">
+          <Routes>
+            {/* Routes publiques */}
+            <Route path="/" element={<><Navbar /><Home /><Footer /></>} />
+            <Route path="/products" element={<><Navbar /><Products /><Footer /></>} />
+            <Route path="/product/:id" element={<><Navbar /><ProductDetails /><Footer /></>} />
+            <Route path="/research" element={<><Navbar /><Research /><Footer /></>} />
+            <Route path="/careers" element={<><Navbar /><Careers /><Footer /></>} />
+            <Route path="/team" element={<><Navbar /><Team /><Footer /></>} />
+            <Route path="/blog" element={<><Navbar /><Blog /><Footer /></>} />
+            <Route path="/blog/:id" element={<><Navbar /><BlogPost /><Footer /></>} />
+            <Route path="/job/:id" element={<><Navbar /><JobDetails /><Footer /></>} />
 
-          {/* Routes admin */}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="products" element={<AdminProducts />} />
-            <Route path="blog" element={<AdminBlog />} />
-            <Route path="careers" element={<AdminCareers />} />
-            <Route path="team" element={<AdminTeam />} />
-            <Route path="settings" element={<AdminSettings />} />
-          </Route>
-        </Routes>
-      </div>
+            {/* Routes admin */}
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<AdminDashboard />} />
+              <Route path="products" element={<AdminProducts />} />
+              <Route path="blog" element={<AdminBlog />} />
+              <Route path="careers" element={<AdminCareers />} />
+              <Route path="team" element={<AdminTeam />} />
+              <Route path="settings" element={<AdminSettings />} />
+            </Route>
+          </Routes>
+        </div>
+      </AdminProvider>
     </Router>
   );
 }
